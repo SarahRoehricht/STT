44276d707a26b7cd7cfc5f4f34ca94391091e145
onurcapci <onurcapci@web.de>
2016-04-14 16:28:28 +0200

    Sphinx Version 2

--- SphinxSTT/src/demo/sphinx/helloworld/HelloWorld.java
+++ SphinxSTT/src/demo/sphinx/helloworld/HelloWorld.java
@@ -15,16 +15,10 @@ import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
 import edu.cmu.sphinx.util.props.PropertyException;
 
-/**
- * A simple HelloWorld demo showing a simple speech application 
- * built using Sphinx-4. This application uses the Sphinx-4 endpointer,
- * which automatically segments incoming audio into utterances and silences.
- */
+
 public class HelloWorld {
 
-	/**
-	 * Main method for running the HelloWorld demo.
-	 */
+
 	public static void main(String[] args) {
 
 		try {
@@ -40,10 +34,9 @@ public class HelloWorld {
 
 			System.out.println("Loading...");
 
-			boolean sendedSpeech;
 
 			int port = 8884; /*args.length == 0 ? 57 : Integer.parseInt(args[0])*/;
-	
+
 
 			ConfigurationManager cm = new ConfigurationManager(url);
 
@@ -52,89 +45,240 @@ public class HelloWorld {
 
 			/* allocate the resource necessary for the recognizer */
 			recognizer.allocate();
-			
-			
-			
+
+
+
 			while(true){
-				
+
 				new Receiver().run(port);
-					
-					if ("#STT#1".equals(Receiver.received)){
 
-						if(microphone.startRecording()){
+				if ("#STT#1".equals(Receiver.received)){
 
+					if(microphone.startRecording()){
+
+						System.out.println ("Say: (Good morning | Hi) ");
+						System.out.println ("Say: (Yes | No) ");
+						System.out.println ("Say: (Take Care | See you) ");
+
+						while(true){
 							System.out.println
-							("Say: (Good morning | Hello | How are you) ");
+							("Start speaking. Press Ctrl-C to quit.\n");
 
-								while(true){
-									System.out.println
-									("Start speaking. Press Ctrl-C to quit.\n");
+							new Receiver().run(port);
 
-									new Receiver().run(port);
+							if ("#STT#0".equals(Receiver.received)){
 
-									if ("#STT#0".equals(Receiver.received)){
+								microphone.stopRecording();
 
-										microphone.stopRecording();
-										
-										/*
-										 * This method will return when the end of speech
-										 * is reached. Note that the endpointer will determine
-										 * the end of speech.
-										 */ 
-										Result result = recognizer.recognize();
+								/*
+								 * This method will return when the end of speech
+								 * is reached. Note that the endpointer will determine
+								 * the end of speech.
+								 */ 
+								Result result = recognizer.recognize();
 
-										if (result != null) {
+								if (result != null) {
 
-											String resultText = result.getBestFinalResultNoFiller();
-											resultText = "#STT#" + resultText;
-											System.out.println("You said: " + resultText + "\n");
+									String resultText = result.getBestFinalResultNoFiller();
+									resultText = "#STT#" + resultText;
+									System.out.println("You said: " + resultText + "\n");
 
-											/*über UDP senden*/
-											DatagramSocket socket;
-											try {
-												socket = new DatagramSocket();
+									/*über UDP senden*/
+									DatagramSocket socket;
+									try {
+										socket = new DatagramSocket();
 
-												byte[] b = resultText.getBytes();
+										byte[] b = resultText.getBytes();
 
-												InetAddress host = InetAddress.getByName("134.103.111.123");
-												//int port2 = 8884;
-												DatagramPacket request = new DatagramPacket(b, b.length, host, port);
-												socket.send(request);
-												System.out.println("Packet versendet an:  " + request.getAddress() + ":" + request.getPort()
-												+ " -> " + new String(request.getData()));
-												sendedSpeech=true;
-											} 
-											catch (SocketException e) {
-												// TODO Auto-generated catch block
-												e.printStackTrace();
-											} 
-											catch (UnknownHostException e) {
-												// TODO Auto-generated catch block
-												e.printStackTrace();
-											} 
-											catch (IOException e) {
-												// TODO Auto-generated catch block
-												e.printStackTrace();
-											}
-										}
-										else {
-											System.out.println("I can't hear what you said.\n");
-										}
+										InetAddress host = InetAddress.getByName("134.103.111.123");
+										//int port2 = 8884;
+										DatagramPacket request = new DatagramPacket(b, b.length, host, port);
+										socket.send(request);
+										System.out.println("Packet versendet an:  " + request.getAddress() + ":" + request.getPort()
+										+ " -> " + new String(request.getData()));
+									} 
+									catch (SocketException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									} 
+									catch (UnknownHostException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									} 
+									catch (IOException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
 									}
-									else{
-										System.out.println("Speak again");
-									}
-									break;	//continue outer;
 								}
-						}
-						else {
-							/*microphone.stopRecording();*/
-							System.out.println("Stop microphone.");
-							recognizer.deallocate();
-							System.exit(1);
+								else {
+									System.out.println("I can't hear what you said.\n");
+								}
+							}
+							else{
+								System.out.println("Speak again");
+							}
+							break;
 						}
 					}
+					else {
+						System.out.println("Microphone doesn't work!");
+						//recognizer.deallocate();
+						System.exit(1);
+					}
 				}
+			}
+				
+				
+			//do{
+			/*	for (int i=0; i<5; i++){
+					
+				new Receiver().run(port);
+
+				if ("#STT#1".equals(Receiver.received)){
+
+					if(microphone.startRecording()){
+
+						System.out.println
+						("Say: (Yes | No) ");
+
+						while(true){
+							System.out.println
+							("Start speaking. Press Ctrl-C to quit.\n");
+
+							new Receiver().run(port);
+
+							if ("#STT#0".equals(Receiver.received)){
+
+								microphone.stopRecording();
+
+								Result result = recognizer.recognize();
+
+								if (result != null) {
+
+									String resultText = result.getBestFinalResultNoFiller();
+									resultText = "#STT#" + resultText;
+									System.out.println("You said: " + resultText + "\n");
+
+									DatagramSocket socket;
+									try {
+										socket = new DatagramSocket();
+
+										byte[] b = resultText.getBytes();
+
+										InetAddress host = InetAddress.getByName("134.103.111.123");
+										//int port2 = 8884;
+										DatagramPacket request = new DatagramPacket(b, b.length, host, port);
+										socket.send(request);
+										System.out.println("Packet versendet an:  " + request.getAddress() + ":" + request.getPort()
+										+ " -> " + new String(request.getData()));
+										//sendedSpeech=true;
+									} 
+									catch (SocketException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									} 
+									catch (UnknownHostException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									} 
+									catch (IOException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+								}
+								else {
+									System.out.println("I can't hear what you said.\n");
+								}
+							}
+							else{
+								System.out.println("Speak again");
+							}
+							break;
+						}
+					}
+					else {
+						System.out.println("Microphone doesn't work!");
+						//recognizer.deallocate();
+						System.exit(1);
+					}
+				}
+				}
+			//}while();
+				
+				
+				
+				new Receiver().run(port);
+
+				if ("#STT#1".equals(Receiver.received)){
+
+					if(microphone.startRecording()){
+
+						System.out.println
+						("Say: (Goodbye | See you tomorrow) ");
+
+						while(true){
+							System.out.println
+							("Start speaking. Press Ctrl-C to quit.\n");
+
+							new Receiver().run(port);
+
+							if ("#STT#0".equals(Receiver.received)){
+
+								microphone.stopRecording();
+ 
+								Result result = recognizer.recognize();
+
+								if (result != null) {
+
+									String resultText = result.getBestFinalResultNoFiller();
+									resultText = "#STT#" + resultText;
+									System.out.println("You said: " + resultText + "\n");
+
+				
+									DatagramSocket socket;
+									try {
+										socket = new DatagramSocket();
+
+										byte[] b = resultText.getBytes();
+
+										InetAddress host = InetAddress.getByName("134.103.111.123");
+										//int port2 = 8884;
+										DatagramPacket request = new DatagramPacket(b, b.length, host, port);
+										socket.send(request);
+										System.out.println("Packet versendet an:  " + request.getAddress() + ":" + request.getPort()
+										+ " -> " + new String(request.getData()));
+										//sendedSpeech=true;
+									} 
+									catch (SocketException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									} 
+									catch (UnknownHostException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									} 
+									catch (IOException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+								}
+								else {
+									System.out.println("I can't hear what you said.\n");
+								}
+							}
+							else{
+								System.out.println("Speak again");
+							}
+							break;
+						}
+					}
+					else {
+						System.out.println("Microphone doesn't work!");
+						//recognizer.deallocate();
+						System.exit(1);
+					}
+				}*/
+		
 		}
 		catch (IOException e) {
 			System.err.println("Problem when loading HelloWorld: " + e);
