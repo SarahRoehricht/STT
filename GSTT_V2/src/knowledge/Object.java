73fc7c5c6645cd2b43082475c5afdaf3debc0cd5
KuKKilicious <clouseyda@yahoo.de>
2017-05-02 01:01:42 +0200

    Object file and package

---
+++ GSTT_V2/src/knowledge/Object.java
@@ -0,0 +1,37 @@
+package knowledge;
+
+
+
+public class Object {
+
+private String name;
+private String color;
+private String location;
+private String size;
+private String weight;
+public Object(String name, String color,String location, String size, String weight){
+	this.name=name;
+	this.color=color;
+	this.location=location;
+	this.size=size;
+	this.weight=weight;
+}
+public String getName() {
+	
+	return name;
+}
+public String getColor() {
+	return color;
+}
+public String getLocation() {
+	return location;
+}
+public String getSize() {
+	return size;
+}
+public String getWeight() {
+	return weight;
+}
+	
+	
+}
